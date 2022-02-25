fd406ea8baa0494488a6c700631e2935837caa6b
Work <noskinav@inter.loc>
2022-02-25 15:58:50 +0300

    change for dz

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,9 +1,6 @@
 import {useState} from 'react';
 
 export const Counter = () => {
-/*     const countState = useState(0);
-    const count = countState[0];
-    const setCount = countState[1]; */
 
     const [count, setCount] = useState(1);
     
